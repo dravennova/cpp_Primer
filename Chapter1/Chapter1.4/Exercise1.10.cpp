d4b5104b9b73d703e7f0cddacf9a3f9660f7ea3c
Draven Nova <draven77medina@hotmail.com>
2023-03-23 14:11:52 -0700

    Completed Exercise10.cpp

--- Chapter1/Chapter1.4/Exercise1.10.cpp
+++ Chapter1/Chapter1.4/Exercise1.10.cpp
@@ -1,21 +1,20 @@
-//Prompt: In addition to the ++ operator that adds 1 to its operand,
-//there is a decrement operator (--) that subtracts 1. Use the decrement 
-//operator to write a while that prints the numbers from ten down to zero.
+//Prompt: In addition to the ++ operator that adds 1 to its operand, there is a decrement operator (--) 
+//that subtracts 1. Use the decrement operator to write a while that prints the 
+//numbers from ten down to zero.
 
 //Compile
+
+
 
 #include <iostream>
 
 int main()
 {
-    int sum = 0, val = 50;
+   int value = 10; 
 
-    while(sum <= 100)
-    {
-        sum += val;
-        --val;
-    }
-    
-    std::cout << "The Sum of teh inclusive 50 to 100 is: " << sum << std::endl;
-    
+   while (value >= 0)
+   {
+    std::cout << value << " ";
+    --value;
+   }
 }
