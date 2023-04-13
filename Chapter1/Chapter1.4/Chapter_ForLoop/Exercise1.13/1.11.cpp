0c00334af34acedc7749dd852ff38a942d060333
Draven Nova <draven77medina@hotmail.com>
2023-04-13 14:41:55 -0700

    Finished 1.11 part of 1.13 exercise

--- Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.11.cpp
+++ Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.11.cpp
@@ -1,1 +1,24 @@
 //Prompt: Rewrite the exercises from § 1.4.1 (p. 13) using for loops.
+
+//Exercise prompt: Write a program that prompts the user for two integers. 
+//Print each number in the range specified by those two integers.
+
+#include <iostream>
+
+int main()
+{
+    int userVal1;
+    int userVal2;
+
+    std::cout << "Enter the first value: ";
+    std::cin >> userVal1;
+    
+    std::cout << "Enter the second value: ";
+    std::cin >> userVal2;
+
+    for(int i = userVal1; i < userVal2 + 1; i++ )
+    {
+        std::cout << i << ", "; 
+    }
+
+}
