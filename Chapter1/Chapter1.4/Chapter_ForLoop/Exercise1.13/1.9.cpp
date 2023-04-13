77288b80a1533c5da721366b173875a0fe41ff3c
Draven Nova <draven77medina@hotmail.com>
2023-04-13 14:23:41 -0700

    finished 1.9 exercise part of 1.13 exercise

--- Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.9.cpp
+++ Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.9.cpp
@@ -1,1 +1,18 @@
 //Prompt: Rewrite the exercises from § 1.4.1 (p. 13) using for loops.
+
+//Exercise Prompt: Write a program that uses a while to sum the numbers from
+//50 to 100
+
+#include <iostream>
+
+int main()
+{
+    int value;
+    int sum = 0;
+
+    for(int value = 50; value <= 100; ++value)
+    {
+        sum+=value;
+    }
+    std::cout << "Sum of 50 to 100 inclusive is: " << sum << std::endl;
+}
