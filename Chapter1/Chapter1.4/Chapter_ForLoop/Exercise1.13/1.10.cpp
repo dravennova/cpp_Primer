56221fdf53e994b1fcaa8e619f63acdc6f151b01
Draven Nova <draven77medina@hotmail.com>
2023-04-13 14:24:05 -0700

    Finished 1.10 exercise part of 1.13 exercise

--- Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.10.cpp
+++ Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.13/1.10.cpp
@@ -1,1 +1,18 @@
 //Prompt: Rewrite the exercises from § 1.4.1 (p. 13) using for loops.
+
+//Exercise Prompt: In addition to the ++ operator that adds 1 to its operand, there is a decrement operator (--) 
+//that subtracts 1. Use the decrement operator to write a while that prints the 
+//numbers from ten down to zero.
+
+#include <iostream>
+
+int main()
+{
+    int value;
+    int sum = 0;
+
+    for (int value = 10; value >= 0; value--)
+    {
+        std::cout << value << ", ";
+    }
+}
