0f397bf40e14acbdcb0ee1217449777e460b65ff
Draven Nova <draven77medina@hotmail.com>
2023-05-03 13:28:12 -0700

    Finished for loop exercises

---
+++ Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.14.cpp
@@ -0,0 +1,5 @@
+//Prompt: Compare and contrast the loops that used a for with those using a while.
+//Are there advantages or disadvantages to using either form?
+
+//While the funtion of a While loop can do the same as a for loop, in a project, have a better
+//readability for functions that you use. 
---
+++ Chapter1/Chapter1.4/Chapter_ForLoop/Exercise1.15.cpp
@@ -0,0 +1,11 @@
+//Prompt: Write programs that contain the common errors discussed in the box on page 16. 
+//Familiarize yourself with the messages the compiler generates.
+
+//Type error:
+int main()
+{
+    char 10; //Type error
+
+    cout << 10 << endl; // Declaration error
+
+}
